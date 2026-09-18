ef3b0f1247c4350e36b72aa906afe820de128c4e
Gunal <gunal.j0907@gmail.com>
2026-09-18 13:31:04 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
 output "primary_bucket_name" {
   description = "Bucket name of primary bucket"
   value       = aws_s3_bucket.pocproject[pocbucket1].bucket_name
+}
 output "distribution_domain" {
   description = "Domain name of cloudfront distribution"
   value       = aws_cloudfront_distribution.poc_distribution.domain_name
