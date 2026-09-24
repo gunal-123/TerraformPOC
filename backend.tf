38de8d818a5322739144cdca98eedb370f0dd302
Gunal <gunal.j0907@gmail.com>
2026-09-24 13:48:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-bucket = "backend-bucket-634541169891-us-east-1-an"
+bucket = "backend-bucket-968894489489-us-east-1-an"
 key = "terraform.tfstate"
 region = "us-east-1"
 use_lockfile = true
